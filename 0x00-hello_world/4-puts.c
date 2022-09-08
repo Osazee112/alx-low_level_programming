096d48cc94de287d2c2802b9c69773dbf297ffec
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 14:49:59 -0700

    put c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print the string in the put function
+ * 
+ * description: using the main function
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+in main(viod)
+{   
+      puts("\"programming is like building a multilingual puzzle");
+      return(0);
+}      
