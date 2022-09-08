a42d6da0e6231a8206e311c62dfeaeb5642a0958
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 15:08:13 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
-in main(void)
+int main(void)
 {   
       puts("\"programming is like building a multilingual puzzle");
       return(0);
