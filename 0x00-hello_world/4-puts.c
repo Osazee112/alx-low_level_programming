8fec80c16045a82f4752cc1449215daf862fa094
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 14:56:19 -0700

    pu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
-in main(viod)
+in main(void)
 {   
       puts("\"programming is like building a multilingual puzzle");
       return(0);
