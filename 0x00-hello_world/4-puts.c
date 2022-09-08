8251d6c66c6de24e9c87e593462ba47a70e15d04
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 15:16:34 -0700

    pot

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main -print the string in the put function
  * 
  * description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
+ * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
