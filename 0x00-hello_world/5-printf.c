b32c48dfc9b30e8b1c4d19648540a7a78500ac09
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 17:07:09 -0700

    fives

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* description: using the main function 
+* this program prints "programming is like bulding a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
