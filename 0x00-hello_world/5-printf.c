ac48511115cdcbf2d45656386aaa088ecffd1db4
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 19:53:56 -0700

    finf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0
+*/
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a pieceof art,\n");
+	return (0);
+}		
