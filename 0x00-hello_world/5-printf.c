e101223df96f9b782b4357299634688e0a72adea
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 18:32:08 -0700

    int

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point 
  *
- * description:using the main function 
- * this program prints "programming is like bulding a multilingual puzzle
  * Return:0
 */
 int main(void)
