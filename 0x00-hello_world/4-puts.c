020a34d30dde271f0b9ebcc0a45aeb85586681fc
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 19:44:01 -0700

    cup

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return:0 
 */
 int main(void)
-{	
+{
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-}		
+}	
+
