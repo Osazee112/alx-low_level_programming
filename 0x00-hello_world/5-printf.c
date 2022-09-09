a43d4c0b938e836d098e51ce4df9d4ca4de39ddb
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-08 18:26:48 -0700

    fake

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-*
-* description:using the main function 
-* this program prints "programming is like bulding a multilingual puzzle
-* Return:0
+ * main - print the string in the put function
+ *
+ * description:using the main function 
+ * this program prints "programming is like bulding a multilingual puzzle
+ * Return:0
 */
 int main(void)
 {
