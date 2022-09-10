e7e05dffb7602b71728b67d27c24d2b8e667076d
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-09 18:46:38 -0700

    use

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main - Entry point
  *  
  * Return: Always 0 
- */
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);   
 }  
