59edfa0904b9090c009b7cdc6f22402739325b9b
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-09 18:09:55 -0700

    ips

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 
+ * Return:Always 0 
  */
 int main(void)
-{
+ {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
-}  
+ }  
