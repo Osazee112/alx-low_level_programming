01f2fb734f5215d41b8af808de9b01f73aff935b
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-09 20:34:24 -0700

    real

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,9 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015- 10- 19",
- * followed by a line, to standard error.
+ * main - prints Entry point
  *
- * Return: Always 1.
+ * Return: Always 1
  */
 int main(void)
 {
