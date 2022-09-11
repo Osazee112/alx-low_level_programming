766753dad8d3fb45053c912ab176a83fa51107c8
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-11 08:05:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
 /*
- * FILE:101-quote.c
+ * File: 101-quote.c
  * Auth: Brennan D Baraban
  */
 
-#include <unistd.h>
+#include <unstdio.h>
 
 /**
- * main - prints Entry point
+ * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
  *
- * Return: Always 1
+ * Return: Alway 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015- 10- 19\n" ,59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
 	return (1);
 }
