3b8517f44b5edd0d7590e5db843446d9fa3e6078
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-12 16:16:31 -0700

    addi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 /**
  * main - printing numbers from 0-9 with commas and space between them
  * Description: using the main function
- * this progam prints "0,1,2,3,4,5,6,7,8,9"
+ * this progam prints " 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 "
  *Return: 0
  */
 int main(void)
