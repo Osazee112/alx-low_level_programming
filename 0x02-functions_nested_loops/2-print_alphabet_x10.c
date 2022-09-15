62410f0a38b90a79ac6c2cd163faae6fd3a04ea8
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-14 18:23:40 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,14 @@
 void print_alphabet_x10(void)
 {
 	char c, i;
-for (i = 0; i <= 9; i++);
-{
-for (c = 'a'; c <= 'z'; c++);
-{
-	_putchar(c);
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
-_putchar('\n');
-}
-}
+		
