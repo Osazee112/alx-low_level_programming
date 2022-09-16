4bfd0d9d1a9faa3b1e2673f78a76cd1237785fd1
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-16 04:58:37 -0700

    first

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or negative(int i);
+int largest_number(int, int, int);
+void print_remainding_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN.H */
