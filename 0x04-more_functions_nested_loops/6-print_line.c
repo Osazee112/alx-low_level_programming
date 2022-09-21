632b26a738f7f1491a3d8f492596ae1fd733e8a9
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-20 17:31:24 -0700

    loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,20 +14,4 @@ void print_line(int n)
 		putchar('_');
 	}
 	putchar('\n');
-}#include <stdio.h>
-#include "main.h"
-
-/** 
- * print_line - prints a straight line
- * @n: parameter
- * Return:returns nothing
- */
-
-void print_line(int n)
-{
-	while (n-- > 0)
-	{
-		putchar('_');
-	}
-	putchar('\n');
 }
