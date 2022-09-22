843117fdcd26a93c9c55cd266ffa4e4ab1d111ba
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-22 14:35:37 -0700

    se

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,8 @@
 
 /**
  * _strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by 
- *
- * @dest: String that will be appende
- *   
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
  * Return: returns poiner to @dest
