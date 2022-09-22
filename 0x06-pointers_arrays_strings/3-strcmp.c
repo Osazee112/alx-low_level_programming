71d1a681eb1dd035482cfa02d8956f6796685d72
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-22 14:59:22 -0700

    se

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  *  @s1: first string
  *  @s2:second string
  *  Return:
- *               returns zero if s1 == s2
- *               returns negative number if s1 < s2
- *               returns positive number if s1 > s2
+ *  returns zero if s1 == s2
+ *  returns negative number if s1 < s2
+ *  returns positive number if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -30,6 +30,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 			i++;
+
 	}
 	return (diff);
 }
