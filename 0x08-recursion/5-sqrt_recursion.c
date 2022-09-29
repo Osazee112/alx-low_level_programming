ad4fc911506b14bd10a4154367edf86de543b33e
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-28 18:43:48 -0700

    g

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int helper(int i, int n)
 {
 	int j;
 
-	if (i && i! = n)
+	if (i * i! = n)
 	{
 		if (i > n)
 		{
@@ -21,4 +21,19 @@ int helper(int i, int n)
 		return (j + 1);
 	}
 	return (0);
+}
+
+/**
+ * _sqrt_recursion - return square root
+ * @n: integer to return
+ * Return: return int of square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (helper(i, n) == n && n! = 1)
+		return (-1);
+	return (helper(i, n);
 }
