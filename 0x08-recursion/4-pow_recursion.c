e75271126e5936894eb25a4a84c9e8913e4f2100
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-09-28 18:04:43 -0700

    g

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - gives power of int
+ * @x: intger to power
+ * @y: power to do
+ * Return: integer value
+ */
+
+int_pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+	{
+		return (x*_pow_recursion(x, y - 1));
+	}
+	return (1);
+}
