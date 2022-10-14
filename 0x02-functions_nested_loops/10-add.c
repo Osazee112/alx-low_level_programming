932b1b268fd5c779d8cf03daee4a1c6e7acdc97f
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-10-14 04:39:57 -0700

    learning

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,18 +5,6 @@
  * program to print out the addition of two numbers
  */
 
-int main()
-{
-	int a,b,n;
-	a = 40;
-	b = 30;
-	n = add(a,b);
-	printf("enter number:%d,%d\n",a,b);
-	scanf("%d,%d\n", &a,&b);
-	printf("return %d\n",add(a,b));
-	return (n);
-}
- int add(int a, int b)
-{
-	return (a + b);
-}
+int add(int a, int b)
+       	return ( a + b);
+
