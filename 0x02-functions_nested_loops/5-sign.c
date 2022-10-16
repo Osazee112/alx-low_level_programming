016669111be526ce7e67a3cb417d751cce36b761
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-10-15 20:57:04 -0700

    nice

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * prin_sign - function to check for a sign of a number
+ * print_sign - function to check for a sign of a number
  * @n: is the int that will use for the argument of  the function
  * Return: 0
  */
