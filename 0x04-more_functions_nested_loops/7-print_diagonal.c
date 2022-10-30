3bfd85ebe9c0edc2029f859c92f6fa8963309b43
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-10-29 19:08:41 -0700

    practise

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * function to print out a diagonal
+ * Return:zero
+ */
+ 
+void print_diagonal(int n)
+{
+	int n;
+	if (n-->0)
+	{
+		_putchar('\');
+	}
+	else 
+	{
+		_putchar("\n");
+	}
+}
