2ecbce27343536275eeacf4a1f2f149659b0189d
Osazee112 <ikponmwenosazeepeter112@mail.com>
2022-10-29 18:31:59 -0700

    practise

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * function that print every minute
+ * Return:zero 
+ */
+
+
+void jack_bauer(void)
+{
+	for ( c = 00:00; c >= 00:09; c++)
+	{
+		printf("\n",c);
+	}
+	for (c = 23:50; c >= 23:59; c++)
+	{
+		printf("\n",c);
+	}
+}
